7229d24005ad0c7f870b0f8adf7cca265f6778fc
Jiaming Guo <j23guo@gmail.com>
2023-06-15 19:23:34 -0400

    Initial Commit

---
+++ CowsAndBulls/CowsAndBullsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowsAndBullsApp.swift
+//  CowsAndBulls
+//
+//  Created by Jiaming Guo on 2023-06-15.
+//
+
+import SwiftUI
+
+@main
+struct CowsAndBullsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
